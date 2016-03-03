d8b1a26d5a35d3f149fc5f086828bedb23d29bd6
per kjeldsen <pok@easv.dk>
2016-03-03 12:45:38 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-$(function(){
-    $('a').on('click', function(){
-        $(this).parent().addClass('current').siblings().removeClass('current');
-             });
-        });
+// Global functions here
 
-// Current menu active js
+// This is changing the color of the selected navigation item - adding and removing class "selected" with the onclick event
+// Using css and calling fx "li.current a" we can change the original color, size, etc so something new with the click - Test it
 $(function(){
-    $('a.menu').click(function(){
-        $('a.menu').removeClass("active");
-            $(this).addClass("active");
-                                });
+    $('ul li a').on('click', function(){
+        $(this).parent().addClass('current').siblings().removeClass('current');
+        });
+    }); 
+
+// Toggling/switching/pushing the contentarea and mobile navigation into sight - and out again : Using toggleClass to add and remove the Class "active"
+
+$(function(){
+    $(".menu-link").click(function(){
+    $("#menu").toggleClass("active");
+    $(".container").toggleClass("active");
             });
+    });
